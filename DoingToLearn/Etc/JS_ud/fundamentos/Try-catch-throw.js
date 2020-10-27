8f236d8507d28116c0b7c78ec3b5b613a07e4d63
neemesis <neemias.camposcruz@gmail.com>
2020-10-26 21:43:02 -0300

    Create Try-catch-throw.js

---
+++ DoingToLearn/Etc/JS_ud/fundamentos/Try-catch-throw.js
@@ -0,0 +1,29 @@
+function tratarErroELancar(erro) {
+    //throw new Error('...')
+    //throw 10
+    //throw true
+    //throw "msg"
+    throw {
+        nome: erro.nome,
+        msg: erro.message,
+        date: new Date
+    }
+}
+
+function imprimirNomeGritando(obj) {
+    try {
+        console.log('É, deu certo ' + obj.nome.toUpperCase() + '!!!'-)
+    } catch (e) {
+        console.log('É, deu errado, amigo')
+        tratarErroELancar(e)
+
+    } finally {
+        console.log('fim\n')
+    }
+}
+
+const obj1 = {nome: 'Neemias'} //sem erro
+const obj2 = {name: 'Campos'} //Com erro 'name'
+
+imprimirNomeGritando(obj1)
+imprimirNomeGritando(obj2)
