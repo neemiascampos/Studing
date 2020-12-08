19cb5c6618baab6259c4d49a82f113438b5dba19
neemesis <neemias.camposcruz@gmail.com>
2020-12-07 21:16:01 -0300

    Create array.js

---
+++ DoingToLearn/Web/JS_ud/Array/array.js
@@ -0,0 +1,13 @@
+console.log(typeof Array)
+console.log(typeof new Array)
+console.log(typeof [])
+
+let aprovados = new Array['Bia', 'Carlos', 'Ana']
+console.log(aprovados)
+
+aprovados = ['Bia', 'Carlos', 'Ana']
+console.log(aprovados[0])
+console.log(aprovados[1])
+console.log(aprovados[2])
+console.log(aprovados[3])
+
