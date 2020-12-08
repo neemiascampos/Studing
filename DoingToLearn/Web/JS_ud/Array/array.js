70f99464b282733bc1f120c1ddf111515af25269
neemesis <neemias.camposcruz@gmail.com>
2020-12-08 01:34:24 -0300

    Update array.js

--- DoingToLearn/Web/JS_ud/Array/array.js
+++ DoingToLearn/Web/JS_ud/Array/array.js
@@ -1,8 +1,9 @@
+// testando funcionalidades
 console.log(typeof Array)
 console.log(typeof new Array)
 console.log(typeof [])
 
-let aprovados = new Array['Bia', 'Carlos', 'Ana']
+let aprovados = new Array('Bia', 'Carlos', 'Ana')
 console.log(aprovados)
 
 aprovados = ['Bia', 'Carlos', 'Ana']
@@ -10,4 +11,24 @@ console.log(aprovados[0])
 console.log(aprovados[1])
 console.log(aprovados[2])
 console.log(aprovados[3])
+
+aprovados[3] = 'Paulo'
+aprovados.push('Abia')
+console.log(aprovados.length)
+
+aprovados[9] = 'rafael'
+console.log(aprovados.length)
+
+console.log(aprovados)
+aprovados.sort()
+console.log(aprovados)
+
+delete aprovados[1]
+console.log(aprovados[1])
+
+aprovados = ['Bia', 'Ana', 'Carlos']
+aprovados.splice(1, 2, 'Elemento1', 'Elemento2')
+console.log(aprovados)
+
+
 
